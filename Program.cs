05561cdb4a6c1171e1681a09f4c180b7a5e409ec
Diederik Mathijs <diederik.mathijs@delaware.pro>
2020-11-06 10:40:12 +0100

    initial dotnet setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_7._0_in_a_nutshell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
