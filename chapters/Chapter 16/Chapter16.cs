6e7500424571fb98c23fd902c3a49c297f6c2dd9
Diederik Mathijs <diederik.mathijs@gmail.com>
2021-01-26 19:58:45 +0100

    Page 687

--- chapters/Chapter 16/Chapter16.cs
+++ chapters/Chapter 16/Chapter16.cs
@@ -3,6 +3,7 @@ using static System.Console;
 using csharp7.infra;
 using System.Threading.Tasks;
 using System.Net;
+using System.Net.Http;
 using System.IO;
 
 namespace csharp7.chapters.Chapter16
@@ -16,6 +17,7 @@ namespace csharp7.chapters.Chapter16
 
         public async override Task RunExamples(){
             await StartWebClientDownloadFile();
+            await ReceiveCookies();
         }
 
         public async Task StartWebClientDownloadFile(){
@@ -32,6 +34,20 @@ namespace csharp7.chapters.Chapter16
                 stream.Position = 0;
 
                 WriteLine(adapter.ReadToEnd());
+            }
+        }
+
+        public async Task ReceiveCookies(){
+
+            var cC = new CookieContainer();
+            var clientHandler = new HttpClientHandler();
+            clientHandler.CookieContainer = cC;
+
+            var client = new HttpClient(clientHandler);
+            await client.GetAsync("https://www.google.com");
+
+            foreach(var cookie in cC.GetCookies(new Uri("https://www.google.com"))){
+                WriteLine(cookie.ToString());
             }
         }
     }
